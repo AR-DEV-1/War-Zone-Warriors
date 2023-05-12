5ede1db93aba35792fff6a77d27865d0a99f5010
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-05-12 09:52:43 +0500

    Create hs.java

---
+++ gameclass/hs.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Game {
+    private JFrame frame;
+    private HomeScreen homeScreen;
+    // ...
+
+    public Game() {
+        // Create main window
+        frame = new JFrame("War Zone Warriors");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        // Create home screen
+        homeScreen = new HomeScreen();
+
+        // Add home screen to main window
+        frame.add(homeScreen);
+
+        // Show main window
+        frame.setVisible(true);
+    }
+
+    // ...
+}
